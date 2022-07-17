f6320611da7304d51ddcefe318df3f73cea8f5b1
nicovalverdi <104444803+nicovalverdi@users.noreply.github.com>
2022-07-17 15:58:36 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+
+function copy() {
+    alert("Autor: Nicolas Agustin Valverdi. EMAIL: nicov35@hotmail.com Templates: https://startbootstrap.com/previews/agency                      cards: https://startbootstrap.com/previews/shop-homepage");
+    }
